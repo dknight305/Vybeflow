59a13fd8e0fc9917a6f41bb40c5abf526e8cfa40
Carlos Sanchez <computerwhiz117@gmail.com>
2025-07-01 16:50:43 -0400

    Made changes

---
+++ src/index.js
@@ -0,0 +1,4 @@
+// Import Bootstrap CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+// Import Bootstrap JS (optional, for dropdowns, modals, etc.)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
